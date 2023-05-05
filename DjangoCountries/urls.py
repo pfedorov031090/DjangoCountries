0bad30a2a1bb4710d1d8667c86874b51d72d9f64
pfedorov031090 <fatpablopaf@yandex.ru>
2023-05-05 13:58:35 +0300

    Модуль 1 и 2 готовы

--- DjangoCountries/urls.py
+++ DjangoCountries/urls.py
@@ -6,6 +6,6 @@ from MainApp import views
 urlpatterns = [
     path('', views.home),
     path('countries-list/', views.countries_list),
-    path('countries-list/<slug:country>/', views.country_page),
+    path('countries-list/<country>/', views.country_page),
     path('languages/', views.languages),
 ]
